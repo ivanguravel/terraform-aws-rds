f1c178e03e68fef089e0a8adbb230e7147ee3f43
izhuravel <ivanguravel@gmail.com>
2023-02-21 21:42:58 +0200

    change vpc logic

--- examples/complete-mysql/main.tf
+++ examples/complete-mysql/main.tf
@@ -170,13 +170,13 @@ resource "aws_default_vpc" "default_vpc" {
 
 # Providing a reference to our default subnets
 resource "aws_default_subnet" "default_subnet_a" {
-  availability_zone = "eu-west-2a"
+  availability_zone = "eu-west-1a"
 }
 
 resource "aws_default_subnet" "default_subnet_b" {
-  availability_zone = "eu-west-2b"
+  availability_zone = "eu-west-1b"
 }
 
 resource "aws_default_subnet" "default_subnet_c" {
-  availability_zone = "eu-west-2c"
+  availability_zone = "eu-west-1c"
 }
